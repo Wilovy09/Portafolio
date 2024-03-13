91c502afd720575d05c5e01ebc2e9faee96539d7
Wilovy09 <srwilovy09@gmail.com>
2024-03-13 17:29:09 -0600

    Agregados nuevos proyectos y reorganizada la estructura de la página de proyectos

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -4,7 +4,7 @@
 
 <template>
     <a :href="projectCard.demo ?? projectCard.github" target="_blank">
-        <div class="max-w-xs cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
+        <div class="mb-4 max-w-xs cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
             <img class="w-full rounded-lg object-cover object-center" :src="projectCard.img ?? '/images/codingIMG.webp'" :alt="projectCard.alt" />
             <p class="my-4 pl-4 font-bold text-gray-500">{{ projectCard.title }}</p>
             <p class="mb-4 ml-4 text-xl font-semibold text-gray-800">{{ projectCard.desc }}</p>
--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -21,29 +21,71 @@
             desc: 'Todos tus links en una página',
             github: 'https://github.com/Wilovy09/Mi-LinkHub-ASTRO',
             demo: 'https://my-linkhub-template.netlify.app/'
-        }
+        },
+        {
+            id: 3,
+            img: '/images/projects/guiadeestudioIMG.webp',
+            alt: 'GuiaDeEstudio',
+            title: 'Guia de estudio',
+            tecnologias: ['HTML', 'CSS', 'JS','Bootstrap', 'jQuery'],
+            desc: 'Guia de estudio para aprender lo básico de HTML, CSS, JS, Bootstrap y jQuery',
+            github: 'https://github.com/Wilovy09/GuiaDeEstudioWeb',
+            demo: 'https://wilovy09.github.io/GuiaDeEstudioWeb/'
+        },
+        {
+            id: 4,
+            img: '/images/projects/crudIMG.webp',
+            alt: 'CRUD',
+            title: 'CRUD',
+            tecnologias: ['Svelte', 'Express', 'Prisma','SQLite'],
+            desc: 'CRUD básico con Svelte, Express, Prisma y SQLite',
+            github: 'https://github.com/Wilovy09/Svelte-Express-Prisma-CRUD',
+        },
+        {
+            id: 5,
+            img: '/images/projects/clonInstagram.webp',
+            alt: 'Instragram',
+            title: 'Replica de Instagram',
+            tecnologias: ['Vue', 'Firebase', 'Tailwind'],
+            desc: 'El reto era replicar las funcionalidades básicas de Instragram.',
+            github: 'https://github.com/Wilovy09/desarrollo-de-software-Replicar-Instragram'
+        },
+        {
+            id: 6,
+            img: '/images/projects/tictactoeIMG.webp',
+            alt: 'TicTacToe',
+            title: 'Tic Tac Toe',
+            tecnologias: ['Vue'],
+            desc: 'TIC-TAC-TOE para aprender VUE.js',
+            github: 'https://github.com/Wilovy09/TicTacToe-en-VUE',
+            demo: 'https://joyful-semifreddo-5b518a.netlify.app/'
+        },
     ];
 </script>
 
 <template>
-    <!-- Trabajos -->
-    <!-- <div class="mx-10">
-        <h1 class="my-8 font-bold text-5xl flex justify-end">Trabajos</h1>
-        <hr>
-    </div>
-    <div class="flex justify-center mt-8">
-        <div class="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-            <ProjectCard v-for="project in projects" :key="project.id" :projectCard="project" class="last-of-type:mb-16"/>
+    <main class="mb-20">
+        <!-- Trabajos -->
+        <!-- <div class="mx-10">
+            <h1 class="my-8 font-bold text-5xl flex justify-end">Trabajos</h1>
+            <hr>
         </div>
-    </div> -->
-    <!-- Proyectos -->
-    <div class="mx-10">
-        <h1 class="my-8 font-bold text-5xl">Proyectos</h1>
-        <hr>
-    </div>
-    <div class="flex justify-center mt-8">
-        <div class="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-            <ProjectCard v-for="project in projects" :key="project.id" :projectCard="project" class="last-of-type:mb-16"/>
+        <div class="flex justify-center mt-8">
+            <div class="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+                <ProjectCard v-for="project in projects" :key="project.id" :projectCard="project" class="last-of-type:mb-16"/>
+            </div>
+        </div> -->
+        <!-- Proyectos -->
+        <div class="mx-10">
+            <h1 class="my-8 font-bold text-5xl">Proyectos</h1>
+            <hr>
         </div>
-    </div>
-</template> 
+        <div class="flex justify-center mt-8">
+            <div class="columns-1 sm:columns-2 lg:columns-3">
+                <ProjectCard v-for="project in projects" 
+                            :key="project.id" 
+                            :projectCard="project" />
+            </div>
+        </div>
+    </main>
+</template>
